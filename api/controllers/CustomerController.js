061b0902e2b2b51ec0a63c96f41bcf3e84ca7b16
VeryGoodCat <very.good.kot@gmail.com>
2017-12-03 20:51:32 +0200

    Add pretty inext table

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -27,5 +27,15 @@ module.exports = {
       });
     });
   },
+
+  index: function(req, res, next) {
+    Customer.find(function foundCustomers (err, customers) {
+      if (err) return next(err);
+
+      res.view({
+        customers: customers
+      });
+    });
+  },
 };
 
